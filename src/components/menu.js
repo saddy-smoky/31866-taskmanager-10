51697f887ed4e3f4d3267620abdc140270fbcd45
Александр Ливитчук <saddy802@gmail.com>
2019-12-16 19:44:24 +0200

    complete module4-task1

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,6 @@
-export const createMenu = () =>
+import {createElement} from "../utils";
+
+const createSiteMenuTemplate = () =>
   `<section class="control__btn-wrap">
           <input
             type="radio"
@@ -27,3 +29,25 @@ export const createMenu = () =>
             >STATISTICS</label
           >
         </section>`;
+
+export default class SiteMenu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    createSiteMenuTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
