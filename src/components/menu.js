f39a60509bb47f8db30e0dbfc7816b23e9c42475
Александр Ливитчук <saddy802@gmail.com>
2019-12-29 17:35:26 +0200

    complete module5-task1

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import AbstractComponent from "./abstract-component";
 
 const createSiteMenuTemplate = () =>
   `<section class="control__btn-wrap">
@@ -30,24 +30,8 @@ const createSiteMenuTemplate = () =>
           >
         </section>`;
 
-export default class SiteMenu {
-  constructor() {
-    this._element = null;
-  }
-
+export default class SiteMenu extends AbstractComponent{
   getTemplate() {
     createSiteMenuTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
